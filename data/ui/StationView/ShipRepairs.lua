dfcb6476406792d53e841994723f59e336c07d41
walterar <walterar2@gmail.com>
2014-03-13 14:14:55 -0300

    Synchro master Pioneer-SS (partial, not # 2709)

--- data/ui/StationView/ShipRepairs.lua
+++ data/ui/StationView/ShipRepairs.lua
@@ -1,6 +1,7 @@
 -- Copyright © 2008-2014 Pioneer Developers. See AUTHORS.txt for details
 -- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
+local Character    = import("Character")
 local Engine       = import("Engine")
 local Lang         = import("Lang")
 local Game         = import("Game")
@@ -10,7 +11,6 @@ local Rand         = import("Rand")
 local InfoGauge    = import("ui/InfoGauge")
 local InfoFace     = import("ui/InfoFace")
 local ModelSpinner = import("UI.Game.ModelSpinner")
-local Character    = import("Character")
 
 local l = Lang.GetResource("ui-core")
 local ui = Engine.ui
